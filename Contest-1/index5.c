67c0bccdf5a47907f3eae6a80591322f48adc03c
MDalamin5 <mdal.amin5@northsouth.edu>
2022-10-21 01:28:06 +0600

    contest

---
+++ Contest-1/index5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b;
+    
+    
+    scanf("%d %d",&a,&b);
+    int res= abs(a-b);
+    printf("%d",res);
+    return 0;
+}
