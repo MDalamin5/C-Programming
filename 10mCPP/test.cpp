16be629b477ad947ae4edc2665a071eba8c0ad62
MDalamin5 <mdal.amin5@northsouth.edu>
2022-11-27 23:19:35 +0600

    cpp

---
+++ 10mCPP/test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    cout << "hello world";
+    string s;
+    cin >> s;
+    cout << s;
+    cout << s.size();
+    int a , bl;
+    cin >> a >> bl;
+    int d = a+bl;
+    cout << d;
+    std::cout << s;
+}
